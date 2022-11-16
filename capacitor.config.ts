c4ab44a1734510c6862c6b31c669a4fe5a7644ea
AleCueto <alecuetocontacto@gmail.com>
2022-11-16 16:22:52 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'harvest_yout_mobile',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
